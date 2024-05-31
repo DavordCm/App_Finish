e20fc00a363b6e91e33e7ac47daa9368398185f0
davordcm <Jhoosman.xd@gmail.com>
2024-05-30 22:25:11 -0500

    RETURN

---
+++ src/main/java/Practica_Idat/App_formulario/controller/PromedioModel.java
@@ -0,0 +1,11 @@
+package Practica_Idat.App_formulario.controller;
+
+import lombok.Data;
+
+@Data
+public class PromedioModel {
+    private Integer ec1;
+    private Integer ec2;
+    private Integer ec3;
+    private Integer ecFinal;
+}
